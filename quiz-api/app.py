9b2321847458c79af92290ac2a65d1e6d6896065
LeoPanEsiee <leo.pan@edu.esiee.fr>
2022-05-27 15:10:37 +0200

    Added authentification

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request
+
+from jwt_utils import build_token
 
 app = Flask(__name__)
 
@@ -11,6 +13,16 @@ def hello_world():
 def GetQuizInfo():
 	return {"size": 0, "scores": []}, 200
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+	payload = request.get_json()
+	if(payload['password'] == "Vive l'ESIEE !"):
+		token = build_token()
+		return {"token": str(token)}, 200
+	else :
+		return '', 401
+	
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
