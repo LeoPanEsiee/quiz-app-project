ec4b0d9f1cebcb7dbe7cf0ae0d3d2506c9a2ce67
LeoPanEsiee <leo.pan@edu.esiee.fr>
2022-05-31 15:46:40 +0200

    pushed new quiz

--- quiz-app/quiz-ui/src/views/NewQuizPage.vue
+++ quiz-app/quiz-ui/src/views/NewQuizPage.vue
@@ -1,5 +1,10 @@
 <template>
   <h1>New Quiz Page</h1>
+  <p>Saisissez votre nom :</p>
+  <input type="text" placeholder="username" v-model="username">
+  <p>{{ username }}</p>
+  <br>
+  <button>GO!</button>
 
 
 </template>
